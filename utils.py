bf2e921ae9e392f692ca7112e12e44a8871109a0
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-08-08 18:48:15 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,33 @@
 # UTILS
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+
+
+def train(model, X_train, X_test, Y_train, Y_test, augment=True,
+          batch_size, epochs):
+  
+  model.compile(loss='categorical_crossentropy', 
+          metrics=['accuracy'],
+          optimizer='rmsprop')
+  
+  if augment:
+    datagen = ImageDataGenerator(
+                              width_shift_range=0.125,
+                              height_shift_range=0.125,
+                              fill_mode="constant",
+                              horizontal_flip=True, cval=0.)
+    datagen.fit(X_train)
+    batch_size = batch_size
+    epochs = epochs
+    history = model.fit(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                        steps_per_epoch=(X_train.shape[0] // batch_size),
+                        epochs=epochs, verbose=1,
+                        validation_data=(X_test, Y_test),)
+    
+  else:
+    history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size, 
+                        verbose=1, validation_data=(X_test, Y_test))
+    
 
 def plot_history(history):
   plt.figure(figsize=(6, 2))
