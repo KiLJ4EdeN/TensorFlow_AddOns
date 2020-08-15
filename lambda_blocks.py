30982ec948e395048a55efc8ccbef84eef66db94
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-08-15 15:22:23 +0430

    Create lambda_blocks.py

---
+++ lambda_blocks.py
@@ -0,0 +1,27 @@
+# TF CUSTOM LAYERS
+def image_gradient(x):
+  # make sure dtype is correct.
+  x = tf.cast(x, dtype=tf.float32)
+  x = tf.image.rgb_to_grayscale(x)
+  dx, dy = tf.image.image_gradients(x)
+  return dx + dy
+  
+gradient_layer = tf.keras.layers.Lambda(image_gradient)
+
+def image_to_hsv(x):
+  # make sure dtype is correct.
+  x = tf.cast(x, dtype=tf.float32)
+  x = tf.image.rgb_to_hsv(x)
+  return x
+
+hsv_layer = tf.keras.layers.Lambda(image_to_hsv)
+
+def sobel_gradient(x):
+  x = tf.cast(x, dtype=tf.float32)
+  x = tf.image.rgb_to_grayscale(x)
+  x = tf.image.sobel_edges(x)
+  # add dx and dy optionally.
+  return x[:, :, :, :, 0] + x[:, :, :, :, 1]
+
+sobel_layer = tf.keras.layers.Lambda(sobel_gradient)
+
