bd40954099178f46d1164e6d5c3c1fd4538a68c9
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-08-16 08:16:35 +0430

    Update lambda_blocks.py

--- lambda_blocks.py
+++ lambda_blocks.py
@@ -1,4 +1,10 @@
 # TF CUSTOM LAYERS
+
+def image_to_gray(x):
+  return tf.image.rgb_to_grayscale(tf.cast(x, dtype=tf.float32))
+
+gray_layer = tf.keras.layers.Lambda(image_to_gray)
+
 def image_gradient(x):
   # make sure dtype is correct.
   x = tf.cast(x, dtype=tf.float32)
