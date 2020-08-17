866ae1095922e342d67dc9731b34630e02f30f28
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-08-17 14:02:46 +0430

    Update linear_regression.py

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
-import numpy as np
 
 # Parameters.
 learning_rate = 0.01
 training_steps = 1000
 display_step = 50
 
-X = np.array([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-              7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-Y = np.array([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-              2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+X = tf.constant([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+                7.042,10.791,5.313,7.997,5.654,9.27,3.1], name='X')
+Y = tf.constant([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+                2.827,3.465,1.65,2.904,2.42,2.94,1.3], name='Y')
 
 # Weight and Bias, initialized randomly.
 W = tf.Variable(tf.random.normal(shape=[1], mean=0.0, stddev=1.0,
