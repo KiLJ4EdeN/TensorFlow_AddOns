9e1286baaf5f38d2aa37dabe82d83ac9f845386f
Abdolkarim Saeedi <parsa592323@gmail.com>
2021-04-07 15:37:46 +0430

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,3 +45,24 @@ def plot_history(history):
   plt.ylabel("loss") 
   plt.xlabel("epoch")
   plt.show()
+          
+          
+class CustomGen(keras.utils.Sequence):
+
+    # get the filenames and the labels
+    def __init__(self, image_filenames, labels, batch_size, size=(128, 128, 3)):
+        self.image_filenames = image_filenames
+        self.labels = labels
+        self.batch_size = batch_size
+        self.size = size
+
+    def __len__(self):
+        return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
+
+    def __getitem__(self, idx):
+        batch_x = self.image_filenames[idx * self.batch_size: (idx + 1) * self.batch_size]
+        batch_y = self.labels[idx * self.batch_size: (idx + 1) * self.batch_size]
+
+        return np.array([
+            resize(imread(str(file_name)), self.size)
+            for file_name in batch_x]) / 255.0, np.array(batch_y)
